22fdb2a62ac84e0edca30ea180c4aeedd9ff5601
Eduardo Bohrer <nbluisrs@gmail.com>
2011-04-29 01:04:49 -0300

    node can efficiently support persistent connections

--- more/persistent/server.js
+++ more/persistent/server.js
@@ -1,13 +1,10 @@
 var http = require('http');
 var url = require('url');
-var queryString = require('querystring');
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	setInterval(500, function() {
-		console.log("more now");
-		response.write("More");
-	});
+	console.log('Received:' + request.url);
+	response.write("Request ok - " + Date.now());
 }).listen(8000, '127.0.0.1');
 
 console.log('Server Started');
